3823b95ecbc11183deeb5e37dca11d6e2225d4a1
Lum Lok Hin Markus <lumlokhin04@gmail.com>
2022-10-11 01:06:03 +0800

    My First Commit

---
+++ script.js
@@ -0,0 +1,81 @@
+const totalScore = {playerScore: 0, computerScore: 0}
+
+function getComputerChoice() {
+  const rpsChoice = ["Rock", "Paper", "Scissors"]
+  const randomChoice = Math.floor(Math.random() * rpsChoice.length)
+  return rpsChoice[randomChoice]
+}
+
+function getResult(playerChoice, computerChoice) { 
+  let score;
+  if (playerChoice == computerChoice) {
+    score = 0
+  } else if (playerChoice == "Rock" && computerChoice == "Scissors") {
+    score = 1
+  } else if (playerChoice == "Paper" && computerChoice == "Rock") {
+    score = 1
+  } else if (playerChoice == "Scissors" && computerChoice == "Paper") {
+    score = 1
+  } else {
+    score = -1
+  }
+    return score 
+  }       
+
+function showResult(score, playerChoice, computerChoice) { 
+  const resultDiv = document.getElementById("result")
+  const handsDiv = document.getElementById("hands")
+  const playerScoreDiv = document.getElementById("player-score")
+  
+  if (score == 1) {
+    resultDiv.innerText = "You Win! 🤗"
+  } else if (score == 0) {
+    resultDiv.innerText = "It's a Draw! 😑"
+  } else { 
+    resultDiv.innerText = "You Lose! 😭"
+  }
+
+  handsDiv.innerText = `👨 ${playerChoice} vs 🤖 ${computerChoice}`
+
+  playerScoreDiv.innerText = `Score ${totalScore["playerScore"]} : ${totalScore["computerScore"]}`
+}
+
+function onClickRPS(playerChoice) {
+  console.log(playerChoice)
+  const computerChoice = getComputerChoice()
+  console.log({computerChoice})
+  const score = getResult(playerChoice, computerChoice)
+  totalScore["playerScore"] += score
+  totalScore["computerScore"] -= score
+  console.log(score)
+  console.log(totalScore)
+  showResult(score, playerChoice, computerChoice)
+}
+
+function playGame() {
+  
+ const rpsButtons =   document.querySelectorAll(".rpsButton")
+
+  rpsButtons.forEach(rpsButton => {
+    rpsButton.onclick = () => onClickRPS(rpsButton.value)
+  })
+
+}
+
+const endGameButton = document.getElementById("endGameButton")
+endGameButton.onclick = () => endGame(totalScore)
+
+function endGame(totalScore) {
+  totalScore["playerScore"] = 0
+  totalScore["computerScore"] = 0
+
+  const resultDiv = document.getElementById("result")
+  const handsDiv = document.getElementById("hands")
+  const playerScoreDiv = document.getElementById("player-score")
+
+  resultDiv.innerText = ''
+  handsDiv.innerText = ''
+  playerScoreDiv.innerText = ''
+}
+
+playGame()
